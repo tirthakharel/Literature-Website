3c9f57d80ba4155dfd27d2914d3cd3622d726747
Tirtha Kharel <tkharel21@gmail.com>
2020-05-13 21:30:57 -0500

    players done

---
+++ literature/backend/Player.js
@@ -0,0 +1,20 @@
+// class definition for player
+function Player(name) {
+  this.hand = [];
+  this.name = name;
+  this.id = null;
+}
+
+Player.prototype.addToHand = function addToHand(card) {
+  this.hand.push(card);
+};
+
+// returns a Set
+Player.prototype.getSets = function getSets() {
+  let Sets = new Set();
+  this.hand.forEach((card) => {
+    Sets.add(card.getSet());
+  });
+
+  return Sets;
+};

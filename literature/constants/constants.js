5fe0f4052bdca81fdb05f995ceec62aed1059eb4
ishaan-rao <ishdr08@gmail.com>
2020-05-13 22:35:24 -0400

    basic card

---
+++ literature/constants/constants.js
@@ -0,0 +1,58 @@
+export const suits = [
+    "Hearts",
+    "Diamonds",
+    "Spades",
+    "Clubs",
+    "Joker",
+]
+
+export const ranks = [
+    "2",
+    "3",
+    "4",
+    "5", 
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+    "Red",
+    "Black",
+]
+
+export const sets = [
+    "Low Hearts",
+    "High Hearts",
+    "Low Diamonds",
+    "High Diamonds",
+    "Low Spades",
+    "High Spades",
+    "Low Clubs",
+    "High Clubs",
+    "Jokers",
+]
+
+export const low = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+]
+
+export const high = [
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+]
+
+
+

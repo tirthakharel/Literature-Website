b85ed1c1b5b85ca96b8d6a43626ac0f20c39d8a1
Tirtha Kharel <tkharel21@gmail.com>
2020-05-14 18:04:02 -0500

    what

--- literature/src/App.js
+++ literature/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Home />
       </header>
     </div>
   );

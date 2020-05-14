5f49e1225b1e04aa98995742d834ec1b0f13ca28
allapk19 <praneeth.alla@gmail.com>
2020-05-13 23:03:45 -0400

    started game.js

---
+++ literature/backend/game.js
@@ -0,0 +1,48 @@
+const Player = require('./player');
+const Deck = require('./deck');
+
+function Game(io) {
+  this.io = io;
+  this.players = [];
+}
+
+Game.prototype.start = function start() {
+  if (this.players.length == 6 || this.players.length == 8) {
+    Deck.deal(this.players);
+    this.players[1].isTurn = true;
+  } else {
+    // show alert (not enough/too many players)
+  }
+}
+
+Game.prototype.addPlayer = function addPlayer(name) {
+  players.push(new Player(name));
+}
+
+Game.prototype.removePlayer = function removePlayer(name) {
+  for (var i = 0; i < this.players.length; i++) {
+    if (this.players[i].name == name) {
+      this.players.splice(i, 1);
+    }
+  }
+}
+
+Game.prototype.ask = function ask(sourcePlayer, targetPlayer, card) {
+  if (sourcePlayer.isTurn) {
+    if (targetPlayer.hasCard(card)) {
+      targetPlayer.removeFromHand(card);
+      sourcePlayer.addToHand(card);
+      // log "took card"
+    } else {
+      sourcePlayer.isTurn = false;
+      targetPlayer.isTurn = true;
+      // log "asked for card"
+    }
+  }
+}
+
+Game.prototype.declare = function declare(player) {
+  if (player.isTurn) {
+    
+  }
+}

c18266f7f793de8273d46dfa135f06b83fcf6e46
Tirtha Kharel <tkharel21@gmail.com>
2020-05-14 18:01:07 -0500

    whats good

--- literature/backend/tests/deck_test.js
+++ literature/backend/tests/deck_test.js
@@ -1,20 +1,20 @@
 const Deck = require('../Deck.js');
 const Player = require('../Player.js');
-const Game = require('../Game.js')
+const Game = require('../Game.js');
 
 let test = new Game('io');
 
-test.addPlayer("p1");
-test.addPlayer("p2");
-test.addPlayer("p3");
-test.addPlayer("p4");
-test.addPlayer("p5");
-test.addPlayer("p6");
+test.addPlayer('p1');
+test.addPlayer('p2');
+test.addPlayer('p3');
+test.addPlayer('p4');
+test.addPlayer('p5');
+test.addPlayer('p6');
 
 test.start();
 
 for (let i = 0; i < test.players.length; i++) {
-    console.log(test.players[i].hand);   
+  console.log(test.players[i].hand);
 }
 
 console.log(test.players[0].getSets());
---
+++ literature/src/components/Home.js
@@ -0,0 +1,15 @@
+import { React, Component } from 'react';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      play: false,
+    };
+  }
+
+  render() {
+    return <div>What's good nig</div>;
+  }
+}

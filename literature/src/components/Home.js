b9847a16e082baa6142c4dc5f355a0f44f7aeeff
Tirtha Kharel <tkharel21@gmail.com>
2020-05-15 13:57:07 -0500

    tried adding row

--- literature/src/components/Home.js
+++ literature/src/components/Home.js
@@ -18,43 +18,50 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <Row className="bg" align="middle" justify="center">
-        <Col span={8} offset={4} style={{ height: '100vh' }}>
-          <Row align="middle" justify="center" style={{ height: '100%' }}>
-            <Tabs defaultActiveKey="1" style={{ width: '75%' }}>
-              <TabPane tab="Join Game" key="1">
-                {<GameForm />}
-              </TabPane>
-              <TabPane tab="Create Game" key="2">
-                {<GameForm />}
-              </TabPane>
-            </Tabs>
-          </Row>
-        </Col>
-        <Col span={8} style={{ height: '100vh' }}>
-          <Row
-            align="middle"
-            justify="center"
-            style={{
-              flexDirection: 'column',
-              height: '100%',
-            }}
-          >
-            <h1 style={{ textAlign: 'center' }}>How To Play</h1>
-            <Collapse id="collapse" style={{ width: '80%' }} accordion>
-              <Panel header="This is panel header 1" key="1">
-                <p>{'hi'}</p>
-              </Panel>
-              <Panel header="This is panel header 2" key="2">
-                <p>{'hi'}</p>
-              </Panel>
-              <Panel header="This is panel header 3" key="3">
-                <p>{'hi'}</p>
-              </Panel>
-            </Collapse>
-          </Row>
-        </Col>
-        <Col span={4} />
+      <Row
+        className="bg"
+        align="middle"
+        justify="center"
+        style={{ flexDirection: 'column' }}
+      >
+        <Row style={{ width: '100%' }}>
+          <Col span={8} offset={4} style={{ height: '100vh' }}>
+            <Row align="middle" justify="center" style={{ height: '100%' }}>
+              <Tabs defaultActiveKey="1" style={{ width: '75%' }}>
+                <TabPane tab="Join Game" key="1">
+                  {<GameForm />}
+                </TabPane>
+                <TabPane tab="Create Game" key="2">
+                  {<GameForm />}
+                </TabPane>
+              </Tabs>
+            </Row>
+          </Col>
+          <Col span={8} style={{ height: '100vh' }}>
+            <Row
+              align="middle"
+              justify="center"
+              style={{
+                flexDirection: 'column',
+                height: '100%',
+              }}
+            >
+              <h1 style={{ textAlign: 'center' }}>How To Play</h1>
+              <Collapse id="collapse" style={{ width: '80%' }} accordion>
+                <Panel header="This is panel header 1" key="1">
+                  <p>{'hi'}</p>
+                </Panel>
+                <Panel header="This is panel header 2" key="2">
+                  <p>{'hi'}</p>
+                </Panel>
+                <Panel header="This is panel header 3" key="3">
+                  <p>{'hi'}</p>
+                </Panel>
+              </Collapse>
+            </Row>
+          </Col>
+          <Col span={4} />
+        </Row>
       </Row>
     );
   }

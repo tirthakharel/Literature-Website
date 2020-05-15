a04b1e08007e04404067bbc5c5c23da88c0ea2b8
ishaan-rao <ishdr08@gmail.com>
2020-05-14 22:47:32 -0400

    home page design

--- literature/src/components/Home.js
+++ literature/src/components/Home.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Row, Col, Collapse } from 'antd';
+import 'antd/dist/antd.css';
+import '../style/Home.css'
+
+const { Panel } = Collapse;
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -10,6 +15,32 @@ export default class Home extends React.Component {
   }
 
   render() {
-    return <div>What's good nig</div>;
+    return <Row className="bg" justify="start">
+      <Col span={12}>
+      
+      </Col>
+      <Col span={12}>
+        <h1>How To Play</h1> 
+        <Row
+            align="middle"
+            justify="center"
+            style={{
+              height: "100%",
+            }}
+          >
+          <Collapse id="collapse" accordion>
+            <Panel header="This is panel header 1" key="1">
+              <p>{"hi"}</p>
+            </Panel>
+            <Panel header="This is panel header 2" key="2">
+              <p>{"hi"}</p>
+            </Panel>
+            <Panel header="This is panel header 3" key="3">
+              <p>{"hi"}</p>
+            </Panel>
+          </Collapse>
+        </Row>
+      </Col>
+    </Row>;
   }
 }

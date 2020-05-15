a14313668e06837fe63b957be247590fbb90ab0c
Tirtha Kharel <tkharel21@gmail.com>
2020-05-14 22:39:19 -0500

    ui changes

--- literature/src/components/Home.js
+++ literature/src/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Row, Col, Collapse, Tabs } from 'antd';
 import 'antd/dist/antd.css';
-import '../style/Home.css'
-import GameForm from './GameForm.js'
+import '../style/Home.css';
+import GameForm from './GameForm.js';
 
 const { Panel } = Collapse;
 const { TabPane } = Tabs;
@@ -17,44 +17,45 @@ export default class Home extends React.Component {
   }
 
   render() {
-    return <Row className="bg" justify="start">
-      <Col span={12}>
-        <Row
-          align="middle"
-          justify="center"
-          >
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Join Game" key="1">
-            <GameForm />
-          </TabPane>
-          <TabPane tab="Create Game" key="2">
-            <GameForm />
-          </TabPane>
-        </Tabs>
-        </Row>
-      </Col>
-      <Col span={12}>
-        <h1>How To Play</h1> 
-        <Row
+    return (
+      <Row className="bg" align="middle" justify="center">
+        <Col span={8} offset={4} style={{ height: '100vh' }}>
+          <Row align="middle" justify="center" style={{ height: '100%' }}>
+            <Tabs defaultActiveKey="1" style={{ width: '75%' }}>
+              <TabPane tab="Join Game" key="1">
+                {<GameForm />}
+              </TabPane>
+              <TabPane tab="Create Game" key="2">
+                {<GameForm />}
+              </TabPane>
+            </Tabs>
+          </Row>
+        </Col>
+        <Col span={8} style={{ height: '100vh' }}>
+          <Row
             align="middle"
             justify="center"
             style={{
-              height: "100%",
+              flexDirection: 'column',
+              height: '100%',
             }}
           >
-          <Collapse id="collapse" accordion>
-            <Panel header="This is panel header 1" key="1">
-              <p>{"hi"}</p>
-            </Panel>
-            <Panel header="This is panel header 2" key="2">
-              <p>{"hi"}</p>
-            </Panel>
-            <Panel header="This is panel header 3" key="3">
-              <p>{"hi"}</p>
-            </Panel>
-          </Collapse>
-        </Row>
-      </Col>
-    </Row>;
+            <h1 style={{ textAlign: 'center' }}>How To Play</h1>
+            <Collapse id="collapse" style={{ width: '80%' }} accordion>
+              <Panel header="This is panel header 1" key="1">
+                <p>{'hi'}</p>
+              </Panel>
+              <Panel header="This is panel header 2" key="2">
+                <p>{'hi'}</p>
+              </Panel>
+              <Panel header="This is panel header 3" key="3">
+                <p>{'hi'}</p>
+              </Panel>
+            </Collapse>
+          </Row>
+        </Col>
+        <Col span={4} />
+      </Row>
+    );
   }
 }
